158a9f8275a2df652ce4b21ac731e50b5fbf44b0
AKOTEGNON Anne-Marie <akotegnonannemarie@gmail.com>
2023-03-17 09:08:17 -0700

    ok

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    char str[] = "Programming is like building a multilingual puzzle\n";
-    puts(str);
-    return 0;
+char str[] = "Programming is like building a multilingual puzzle\n";
+   
+puts(str);
+return(0);
 }
