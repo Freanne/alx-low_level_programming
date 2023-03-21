7d12d34b5489c3b725e107b25cab402fa568f8f7
AKOTEGNON Anne-Marie <akotegnonannemarie@gmail.com>
2023-03-21 06:28:10 -0700

    ok

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include<stdio.h>
+#include<stdlib.h>
+#include <unistd.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet_x10(void)
+{
+char letter = 'a';
+
+while (letter <= 'z')
+{
+int i=0;
+
+while(i<10)
+{
+_putchar(letter);
+i++;
+}
+letter++;
+_putchar('\n');
+}
