0020166321d1d68a77a2b680a3f7e07ad55bf369
AKOTEGNON Anne-Marie <akotegnonannemarie@gmail.com>
2023-03-21 17:36:39 -0700

    ok

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+void positive_or_negative(int i)
+{
+if (n > 0)
+{
+printf(" is positive\n");
+}
+else if (n == 0)
+{
+printf(" is zero\n");
+}
+else
+{
+printf(" is negative\n");
+}
+}
