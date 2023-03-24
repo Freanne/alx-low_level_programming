8bf862cd4adf144a1762ffe2ffe907c37731aa86
AKOTEGNON Anne-Marie <akotegnonannemarie@gmail.com>
2023-03-23 17:03:10 -0700

    ok

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,24 @@
+#include <stydio.h>
+#include <stdlib.h>
+/** main - check the code
+ *
+ * Return : void.
+ */
+
+int main(void)
+{
+int i;
+
+for (i = 1; i <= 100; i++)
+{
+if (i % 3 == 0 && i % 5 == 0)
+printf("FizzBuzz ");
+else if (i % 3 == 0)
+printf("Fizz ");
+else if (i % 5 == 0)
+printf("Buzz ");
+else
+printf("%d ", i);
+}
+return (0);
+}
