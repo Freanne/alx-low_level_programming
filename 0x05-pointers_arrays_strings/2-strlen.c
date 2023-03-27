485cae4bb4d8d9399518cd5be550ba7c6fcbe017
AKOTEGNON Anne-Marie <akotegnonannemarie@gmail.com>
2023-03-27 12:58:18 -0700

    ok

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * _strlen - returns the length of a string.
+ *@s:The characteres to be checked.
+ *
+ * Return: a and b.
+ */
+
+
+int _strlen(char *s)
+{
+return strlen(*s);
+}
