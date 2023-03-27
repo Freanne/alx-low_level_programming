b0515d9337cbae1fc9e6b9acd64cf3d5dd982444
AKOTEGNON Anne-Marie <akotegnonannemarie@gmail.com>
2023-03-27 14:37:34 -0700

    ok

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+/**
+ * puts2 - prints every other character of a string,
+ * starting with the first character,
+ * followed by a new line.
+ *@str:The string to be checked.
+ *
+ *
+ * Return: character of s.
+ */
+
+
+void puts2(char *str)
+{
+for (int i = 0; i<strlen(str); i += 2)
+{
+printf("%c",str[i]);
+}
+printf("\n");
+}
