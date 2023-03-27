a4b45e0055bb2866cce075a9fe732e4f23d4e95a
AKOTEGNON Anne-Marie <akotegnonannemarie@gmail.com>
2023-03-27 12:25:05 -0700

    ok

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,7 @@
 /**
  * reset_to_98 - Takes a pointer to an int as parameter
  * and updates the value it points to to 98
- * @n:the numbers to be checked 
+ * @n:the numbers to be checked
  * Return: 98.
  */
 
