adbae5e85862f864af866f46e23a820d558ff24a
AKOTEGNON Anne-Marie <akotegnonannemarie@gmail.com>
2023-04-05 10:53:07 -0700

    ok

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -13,10 +13,13 @@ void _print_rev_recursion(char *s)
 {
 int length = strlen(s), i;
 
-for (i = 0; i < length / 2; i++)
-{
-char temp = s[i];
-s[i] = s[length - i - 1];
-s[length - i - 1] = temp;
-}
+if (length <= 1)
+return ;
+
+char temp = s[0];
+s[0] = s[length  - 1];
+s[length - 1] = temp;
+
+
+_print_rev_recursion(s + 1)
 }
