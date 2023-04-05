d2e794ac20589748140906e538ae162fc8e3112a
AKOTEGNON Anne-Marie <akotegnonannemarie@gmail.com>
2023-04-05 13:00:37 -0700

    ok

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strlen_recursion - The length of a string .
+ * @s : The character to be printed.
+ *
+ * Return : The lenght of a string.
+ */
+
+int _strlen_recursion(char *s)
+{
+if *s != '\0')
+return (0);
+else
+return 1 + _strlen_recursion(s + 1);
+}
