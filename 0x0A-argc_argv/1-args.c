ca441d0317c6f8e8964e706b434c337335a3eeb6
AKOTEGNON Anne-Marie <akotegnonannemarie@gmail.com>
2023-04-10 13:41:35 -0700

    ok

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * main - Entry point
+ * @argc : The number of arguments who programe take
+ *
+ * Return :Always 0.
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+printf("%d\n", argc - 1 );
+return (0);
+
+
+}
