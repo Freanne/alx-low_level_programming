95eeb6e2ddc1316cb9765139ccb2bde3799f606e
AKOTEGNON Anne-Marie <akotegnonannemarie@gmail.com>
2023-04-13 16:58:05 -0700

    ok

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <sdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * str_concat - Concatenates two strings.
+ * @s1 : The fistr string
+ * @s2 : The twice string.
+ *
+ * Return : Pointer should point to a newly allocated space contain0s s1 and s1.
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
+
+int len1 = strlen(s1), len2 = strlen(s2);
+char *ch1 = malloc(len1 * sizeof(char)), ch2 = malloc(len2 * sizeof(char));
+
+if (ch1 == NULL || ch2 == NULL)
+{
+free(ch1);
+free(ch2);
+return (NULL);
+
+}
+strcpy(ch1, s1);
+strcpy(ch2, s2);
+
+char *chaine = malloc((len1 + len2) * sizeof(char));
+
+if (chaine == NULL)
+{
+free(ch1);
+free(ch2);
+retrn (NULL);
+
+
+}
+strcpy(chaine, ch1);
+strcat(chaine, ch2);
+
+return (chaine);
+
+
+
+
+}
