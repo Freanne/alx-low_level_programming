b3d0d4eca455ece438222e64b8fe032b432bc128
AKOTEGNON Anne-Marie <akotegnonannemarie@gmail.com>
2023-04-13 16:18:10 -0700

    ok

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,35 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 /**
- * create_array - Creates an array of chars
- * and initializes it with a specific char.
- * @size : The number to be checked.
- * @c : The character to be checked.
- *
- * Return : a pointer to the created array, or NULL if it fails
  *
  *
  *
- */
-
-char *create_array(unsigned int size, char c)
-{
-char *tab;
-unsigned int i;
-
-if (size == 0)
-return (NULL);
-
-tab = malloc (size * sizeof(char));
-
-if (tab == NULL)
-return (NULL);
-
-for (i = 0; i < size; i++)
-tab[i] = c;
-
-return (tab);
-
-}
+ *
+ *
+ *
+ *
---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _strdup - Reurns a pointer to a newly allocated space in memory,
+ * which contains a copy of the string given as a parameter
+ * @str : The string to be duplicated.
+ *
+ * Return : pointer to the duplicated string.
+ *
+ */
+
+
+char *_strdup(char *str)
+{
+int length = strlen(str);
+char *chaine = malloc(length * sizeof(char));
+
+
+chaine = strcpy(chaine, s);
+return (chaine);
+free(chaine);
+if (str == NULL)
+return (NULL);
+
+}
