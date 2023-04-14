e3bfc36a23f481f3e182a50bd578b26b7f0d9782
AKOTEGNON Anne-Marie <akotegnonannemarie@gmail.com>
2023-04-13 17:04:14 -0700

    ok

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,7 +16,7 @@ char *str_concat(char *s1, char *s2)
 
 int len1 = strlen(s1), len2 = strlen(s2);
 char *ch1 = malloc(len1 * sizeof(char)), *ch2 = malloc(len2 * sizeof(char));
-
+char *chaine = malloc((len1 + len2) * sizeof(char));
 
 if (s1 == NULL)
 s1 = "";
@@ -33,13 +33,13 @@ return (NULL);
 strcpy(ch1, s1);
 strcpy(ch2, s2);
 
-char *chaine = malloc((len1 + len2) * sizeof(char));
+
 
 if (chaine == NULL)
 {
 free(ch1);
 free(ch2);
-retrn (NULL);
+return (NULL);
 
 
 }
