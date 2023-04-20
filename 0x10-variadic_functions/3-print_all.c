f651c904bb16ea164c0d51434c50990b743d8fed
AKOTEGNON Anne-Marie <akotegnonannemarie@gmail.com>
2023-04-20 16:07:09 -0700

    ok

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -45,6 +45,9 @@ case 'f':
 printf("%f",(float)va_arg(args, double));
 break;
 case 's':
+if (va_args(args, char *) == NULL)
+printf("(nil)");
+else
 printf("%s", va_arg(args, char *));
 break;
 default:
