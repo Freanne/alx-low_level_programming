80280f6705c45a6bbb9707848b021a6c5b791961
AKOTEGNON Anne-Marie <akotegnonannemarie@gmail.com>
2023-04-20 14:25:28 -0700

    ok

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -5,14 +5,13 @@
 
 /**
  * sum_them_all - The sum of all its parametrs
- * @n : the parameters
+ * @n : The number of integer for the function
  *
  * Return : The sum of all parameters
  */
 
 int sum_them_all(const unsigned int n, ...)
 {
-
 unsigned int i;
 int sum = 0, tmp;
 va_list arg;
