ca4b7512690f116bceed3865f05091dc6d10af51
AKOTEGNON Anne-Marie <akotegnonannemarie@gmail.com>
2023-04-27 23:37:51 -0700

    printed list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,7 +7,8 @@
  * print_list - Prints all elements of a linked list.
  * @h : The pointer of the list_t to print.
  *
- * Return : The number of nodes printed.
+ *  Return: The number of nodes printed.
+ *         If the linked list is NULL, returns 0.
  */
 size_t print_list(const list_t *h)
 {
