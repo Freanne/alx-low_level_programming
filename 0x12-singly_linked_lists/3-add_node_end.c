de4e78374cac6ffc452db6d38df9868b31213c5e
AKOTEGNON Anne-Marie <akotegnonannemarie@gmail.com>
2023-04-27 23:19:57 -0700

    adding node at the end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -11,38 +11,40 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-    list_t *new_node, *current;
+list_t *new_node, *current;
 
-    if (str == NULL)
-        return (NULL);
+if (str == NULL)
+return (NULL);
 
     /* Allocate memory for the new node */
-    new_node = malloc(sizeof(list_t));
-    if (new_node == NULL)
-        return (NULL);
+new_node = malloc(sizeof(list_t)
+if (new_node == NULL)
+return (NULL);
 
-    /* Initialize the new node */
-    new_node->str = strdup(str);
-    if (new_node->str == NULL) {
-        free(new_node);
-        return (NULL);
-    }
-    new_node->len = strlen(str);
-    new_node->next = NULL;
+/* Initialize the new node */
+new_node->str = strdup(str);
+if (new_node->str == NULL)
+{
+free(new_node);
+return (NULL);
+}
+new_node->len = strlen(str);
+new_node->next = NULL;
 
-    /* If the list is empty, make the new node the head */
-    if (*head == NULL) {
-        *head = new_node;
-    } else {
-        /* Traverse the list to find the last node */
-        current = *head;
-        while (current->next != NULL) {
-            current = current->next;
-        }
-        /* Append the new node to the end of the list */
-        current->next = new_node;
-    }
-
-    return (new_node);
+/* If the list is empty, make the new node the head */
+if (*head == NULL)
+{
+*head = new_node;
+}
+else
+{
+current = *head;
+while (current->next != NULL)
+{
+current = current->next;
+}
+current->next = new_node;
+}
+return (new_node);
 }
 
