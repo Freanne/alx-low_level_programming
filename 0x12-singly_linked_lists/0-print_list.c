ca9f460c7c4652fdea099e18a16f792ecc2dc1ac
AKOTEGNON Anne-Marie <akotegnonannemarie@gmail.com>
2023-04-27 21:30:59 -0700

    print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,10 +14,10 @@ size_t print_list(const list_t *h)
 size_t n = 0;
 while (h != NULL)
 {
-	if (h->str == NULL)
-		printf("[0] (nil)");
-	else
-		printf("[%u] %s", h->len, h->str);
+if (h->str == NULL)
+printf("[0] (nil)");
+else
+printf("[%u] %s", h->len, h->str);
 
 n++;
 }
