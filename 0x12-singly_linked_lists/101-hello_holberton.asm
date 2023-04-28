0b446f40a4414ae5aceb0ebbfe1ab039bcd41143
AKOTEGNON Anne-Marie <akotegnonannemarie@gmail.com>
2023-04-28 14:07:13 -0700

    hellos hoberlton

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,22 +1,8 @@
-section .data
-    message db 'Hello, Holberton', 0
-    format db '%s\n', 0
-
-section .text
-    global main
-
+_global main
+	extern	printf
 main:
-    push rbp
-    mov rbp, rsp
-
-    sub rsp, 16          ; reserve space for printf arguments
-    lea rdi, [message]   ; set first printf argument (string message)
-    lea rsi, [format]    ; set second printf argument (format string)
-    xor eax, eax         ; clear rax (return value)
-    call printf          ; call printf function
-    add rsp, 16          ; clean up the stack
-
-    mov rsp, rbp
-    pop rbp
-    ret
-
+	now	edi, format
+	xor 	eax, eax
+	call 	printf
+	mov	eax, 0
+format: db 'Hello, Hoberlton\n', 0
