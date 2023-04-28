85713d78278c499758ec40709a3627f92e122414
AKOTEGNON Anne-Marie <akotegnonannemarie@gmail.com>
2023-04-27 21:38:32 -0700

    print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,10 +15,11 @@ size_t n = 0;
 while (h != NULL)
 {
 if (h->str == NULL)
-printf("[0] (nil)");
+printf("[0] (nil)\n");
 else
-printf("[%u] %s", h->len, h->str);
+printf("[%u] %s\n", h->len, h->str);
 
+h = h->next;
 n++;
 }
 return (n);
