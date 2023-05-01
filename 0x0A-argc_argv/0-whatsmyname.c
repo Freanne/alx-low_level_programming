4e25a84ead1b51f973acf3c9e4efa400582ea5a3
AKOTEGNON Anne-Marie <akotegnonannemarie@gmail.com>
2023-05-01 05:00:12 -0700

    -ok

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,11 +6,10 @@
  *@argc : The number of command-line arguments.
  *@argv : An array of pointers to the arguments.
  *
- * 
- *Return : Always 0.
+ *Return: Always 0.
  */
 
-int main(int argc  __attribute__((unused)) , char **argv)
+int main(int argc  __attribute__((unused)), char **argv)
 {
 printf("%s\n", argv[0]);
 
