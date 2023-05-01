b3d605c02a1fa77715f868249e253fca8041b388
AKOTEGNON Anne-Marie <akotegnonannemarie@gmail.com>
2023-05-01 04:29:43 -0700

    ok

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,32 +3,6 @@
 #include <string.h>
 
 /**
- * _sqrt_recursion - calculates the natural square root of a number
- * @n: the number to calculate the square root of
- *
- * Return: the natural square root of n,
- * or -1 if n does not have a natural square root
- */
-int _sqrt_recursion(int n)
-{
-int root = _sqrt_helper(n, 0, n);
-
-if (n < 0)
-{
-return (-1);
-}
-
-if (n == 0 || n == 1)
-{
-return (n);
-}
-
-
-
-return (root);
-}
-
-/**
  * _sqrt_helper - helper function to recursively find the square root
  * @n: the number to calculate the square root of
  * @start: the starting value for the search range
@@ -59,4 +33,31 @@ return (_sqrt_helper(n, start, mid - 1));
 
 return (-1);
 }
+
+/**
+ * _sqrt_recursion - calculates the natural square root of a number
+ * @n: the number to calculate the square root of
+ *
+ * Return: the natural square root of n,
+ * or -1 if n does not have a natural square root
+ */
+int _sqrt_recursion(int n)
+{
+int root = _sqrt_helper(n, 0, n);
+
+if (n < 0)
+{
+return (-1);
+}
+
+if (n == 0 || n == 1)
+{
+return (n);
+}
+
+
+
+return (root);
+}
+
 
