3466052425d6611e38161213a536736344d4fc0c
AKOTEGNON Anne-Marie <akotegnonannemarie@gmail.com>
2023-05-02 16:51:21 -0700

    done

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,6 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "lists.h"
 
 /**
@@ -11,13 +8,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
-size_t n = 0;
+	size_t n = 0;
 
-while (h)
-{
-printf("%d\n", h->n);
-n++;
-h = h->next;
-}
-return (n);
+	while (h)
+	{
+		printf("%d\n", h->n);
+		n++;
+		h = h->next;
+	}
+	return (n);
 }
