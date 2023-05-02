c1ab278fc63fdd26fdbb3703f4c6850356e280e9
AKOTEGNON Anne-Marie <akotegnonannemarie@gmail.com>
2023-05-02 16:28:30 -0700

    ok

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
- * Prints the name of the file it was compiled from,
- * followed by a new line
+ * main - Entry point
+ *
+ * Return: 0.
  */
 int main(void)
 {
