9c155c0f96a2a11150c7bb60d723eb9f59f7117f
AKOTEGNON Anne-Marie <akotegnonannemarie@gmail.com>
2023-05-08 15:37:36 -0700

    done

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,52 +1,52 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include "main.h"
 #include <unistd.h>
-
+#include "main.h"
 /**
- * read_textfile - Reads a text file and prints
- * it to the POSIX standard output.
- *@filename: The name of the file to read
- *@letters: The numbers of letters it should read and print.
- *Return: The actual number of letters it could read and print.
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ *
+ * @filename: the name of the file to read
+ * @letters: the number of letters to read and print
+ *
+ * Return: the actual number of letters read and printed, or 0 on failure
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	FILE *file = fopen(filename, "r");
-	char *buffer = malloc(sizeof(char) * (letters + 1));
-	ssize_t nb_read = fread(buffer, sizeof(char), letters, file);
-	ssize_t nb_written = write(STDOUT_FILENO, buffer, nb_read);
+int fd;
+ssize_t nread, nwritten = 0;
+char *buf;
 
-	if (filename == NULL)
-		return (0);
-	if (file == NULL)
-		return (0);
+if (filename == NULL)
+return (0);
 
-	if (buffer == NULL)
-	{
-		fclose(file);
-		return (0);
-	}
+fd = open(filename, O_RDONLY);
+if (fd == -1)
+return (0);
 
-	if (nb_read <= 0)
-	{
-		fclose(file);
-		free(buffer);
-		return (0);
-	}
+buf = malloc(sizeof(char) * (letters + 1));
+if (buf == NULL)
+return (0);
 
-	buffer[nb_read] = '\0';
-	if (nb_written < nb_read)
-	{
-		fclose(file);
-		free(buffer);
-		return (0);
-	}
-	fclose(file);
-	free(buffer);
-
-	return (nb_read);
+nread = read(fd, buf, letters);
+if (nread == -1)
+{
+free(buf);
+close(fd);
+return (0);
 }
+
+nwritten = write(STDOUT_FILENO, buf, nread);
+if (nwritten == -1 || nwritten != nread)
+{
+free(buf);
+close(fd);
+return (0);
+}
+
+free(buf);
+close(fd);
+
+return (nwritten);
+}
+
