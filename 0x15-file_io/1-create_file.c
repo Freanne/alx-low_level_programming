aea0197e81d45ee263a473e44e5f75599a5a952e
AKOTEGNON Anne-Marie <akotegnonannemarie@gmail.com>
2023-05-08 15:10:40 -0700

    done

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,43 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+/**
+ * create_file - creates a file with the specified name and content
+ *
+ * @filename: the name of the file to create
+ * @text_content: a NULL terminated string to write to the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+int fd;
+mode_t mode = S_IRUSR | S_IWUSR; /* rw------- */
+ssize_t len = 0;
+
+if (filename == NULL)
+return (-1);
+
+fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, mode);
+if (fd == -1)
+return (-1);
+
+if (text_content != NULL)
+{
+len = strlen(text_content);
+if (write(fd, text_content, len) != len)
+{
+close(fd);
+return (-1);
+}
+}
+
+close(fd);
+return (1);
+}
+
